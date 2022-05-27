cbbf4a3cf938a3ffe26e8dbfd89c838d84010ef5
Jacob Alford - MBP <jacob.alford@me.com>
2022-05-27 14:28:08 -0600

    Use M for matrix, V for vector

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,9 @@ import * as AbGrp from './Commutative'
 import * as C from './Complex'
 import * as Conj from './Conjugate'
 import * as InPrSp from './InnerProductSpace'
-import * as MC from './MatrixC'
+import * as M from './MatrixC'
 import * as Mod from './Module'
-import * as VC from './VectorC'
+import * as V from './VectorC'
 import * as VecSpc from './VectorSpace'
 
 import * as MatN from './numberMat'
@@ -14,4 +14,4 @@ import * as MatC from './complexMat'
 import * as VecN from './numberVec'
 import * as VecC from './complexVec'
 
-export { AbGrp, C, Conj, InPrSp, MC, Mod, VC, VecSpc, MatN, MatC, VecN, VecC }
+export { AbGrp, C, Conj, InPrSp, M, Mod, V, VecSpc, MatN, MatC, VecN, VecC }
