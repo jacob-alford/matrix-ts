93f16e857f7b2c455e754caff90a7ef73d4a5708
Jacob Alford - MBP <jacob.alford@me.com>
2022-06-25 11:48:17 -0600

    Update test name

--- src/__tests__/Univariate.test.ts
+++ src/__tests__/Univariate.test.ts
@@ -1,7 +1,7 @@
 import * as Stat from '../Univariate'
 
-describe('Multivariate', () => {
-  it('calculates a covariance matrix', () => {
+describe('Univariate', () => {
+  it('calculates a mean', () => {
     const sample: Stat.Sample = [73, 73, 76, 77, 81, 100]
 
     const mean = Stat.mean(sample)
